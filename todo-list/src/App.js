958ada49478c78911cc8a96cb8d47d530b8fc53e
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-27 13:52:05 +0530

    update App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'
+import Todos from './MyComponents/Todos'
+import Footer from './MyComponents/Footer'
+import Todo from './MyComponents/Todo'
 
 function App() {
   return (
     <>
+    <Header/>
+    <Todos/>
+    <Todo/>
+    <Footer/>
   </>
   );
 }
