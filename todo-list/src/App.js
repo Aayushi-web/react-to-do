a3cb22c6f571d7ae871edf5a97f94c96cb5558a5
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-27 14:44:41 +0530

    changes

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -8,7 +8,7 @@ import TodoItem from './MyComponents/TodoItem'
 function App() {
   return (
     <>
-    <Header title="MyToDo"/>
+    <Header title="MyToDo " searchBar= {true}/>
     <Todos/>
     <TodoItem/>
     <Footer/>
